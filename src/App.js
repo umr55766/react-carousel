ac434839dcf6eca464f0736f3a753fdc8c44df5e
Umair <umr55766@gmail.com>
2020-12-10 22:03:37 +0530

    Add basic cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+
 import './App.css';
+import {getProductsData} from "./api/products";
 
 function App() {
+  const [products, setProducts] = useState(getProductsData());
+  const [focusProductIndex, setFocusProductIndex] = useState(1);
+  const [previousFocusProductIndex, setPreviousFocusProductIndex] = useState(0);
+  const [nextfocusProductIndex, setNextFocusProductIndex] = useState(2);
+
+  useEffect(() => {
+    setPreviousFocusProductIndex((focusProductIndex-1)%products.length);
+    setNextFocusProductIndex((focusProductIndex+1)%products.length);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setFocusProductIndex((focusProductIndex-1)%products.length)}>Previous</button>
+
+        <div style={{"border": "1px solid #eee", "margin": "10px", "padding": "10px", "border-radius": "5px", "display": "flex", "flex-direction": "column"}}>
+          <label style={{"text-align": "left"}}>{products[focusProductIndex].category}</label>
+          <img src={products[focusProductIndex].image}/>
+          <label>{products[focusProductIndex].title.length > 10 ? products[focusProductIndex].title.substr(0, 18)+"..." : products[focusProductIndex].title}</label>
+          <label style={{"text-align": "left"}}>{products[focusProductIndex].price}</label>
+        </div>
+
+        <div style={{"border": "1px solid #eee", "margin": "10px", "padding": "10px", "border-radius": "5px", "display": "flex", "flex-direction": "column"}}>
+          <label style={{"text-align": "left"}}>{products[previousFocusProductIndex].category}</label>
+          <img src={products[previousFocusProductIndex].image}/>
+          <label>{products[previousFocusProductIndex].title.length > 10 ? products[previousFocusProductIndex].title.substr(0, 18)+"..." : products[previousFocusProductIndex].title}</label>
+          <label style={{"text-align": "left"}}>{products[previousFocusProductIndex].price}</label>
+        </div>
+
+        <div style={{"border": "1px solid #eee", "margin": "10px", "padding": "10px", "border-radius": "5px", "display": "flex", "flex-direction": "column"}}>
+          <label style={{"text-align": "left"}}>{products[nextfocusProductIndex].category}</label>
+          <img src={products[nextfocusProductIndex].image}/>
+          <label>{products[nextfocusProductIndex].title.length > 10 ? products[nextfocusProductIndex].title.substr(0, 18)+"..." : products[nextfocusProductIndex].title}</label>
+          <label style={{"text-align": "left"}}>{products[nextfocusProductIndex].price}</label>
+        </div>
+
+        <button onClick={() => setFocusProductIndex((focusProductIndex+1)%products.length)}>Next</button>
       </header>
     </div>
   );
---
+++ src/api/products.js
@@ -0,0 +1,46 @@
+export const getProductsData = () => {
+  return [
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snac tac Masala Innt Noodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snac tac Masala I Noodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snac la Instant Noodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snac tac MasalNoodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snac tac Masala Instant s 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Snsala Instant Noodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    },
+    {
+      "image": "https://www.jiomart.com/images/product/150x150/491551662/snac-tac-masala-instant-noodles-300-g-0-20201207.jpg",
+      "title": "Masala Instant Noodles 300 g",
+      "price": 35.00,
+      "category": "Snacks"
+    }
+  ]
+};
